77aae462b4bdb2f35a446cf01929bb5c68242322
Alek Pialucha <pialuchaa@btinternet.com>
2023-07-06 13:00:14 +0100

    light/dark toggle

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,39 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+console.log("test")
+
+const light = document.querySelector("#light")
+const dark = document.querySelector("#dark")
+
+light.addEventListener("click", (e) => {
+  document.querySelectorAll(".primary").forEach(el => {
+    el.style.color = "#151515"
+  })
+  document.querySelectorAll(".primary-border").forEach(el => {
+    el.style.borderColor = "#151515"
+  })
+  document.querySelectorAll(".secondary").forEach(el => {
+    el.style.color = "#ebebeb"
+  })
+  document.querySelectorAll(".secondary-background").forEach(el => {
+    el.style.backgroundColor = "#ebebeb"
+  })
+  dark.classList.toggle("current-toggle")
+  light.classList.toggle("current-toggle")
+
+});
+
+dark.addEventListener("click", (e) => {
+  document.querySelectorAll(".primary").forEach(el => {
+    el.style.color = "#ebebeb"
+  })
+  document.querySelectorAll(".primary-border").forEach(el => {
+    el.style.borderColor = "#ebebeb"
+  })
+  document.querySelectorAll(".secondary").forEach(el => {
+    el.style.color = "#151515"
+  })
+  document.querySelectorAll(".secondary-background").forEach(el => {
+    el.style.backgroundColor = "#151515"
+  })
+  dark.classList.toggle("current-toggle")
+  light.classList.toggle("current-toggle")
+});
